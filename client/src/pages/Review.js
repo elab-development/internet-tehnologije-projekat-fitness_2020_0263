adcc4df3a92e1c0b5271685200bb50ff57d440c6
ivanaarsic <ivana.arsic83@gmail.com>
2024-08-29 15:04:36 +0200

    review vidljiv samo ulogovanim korisnicima

--- client/src/pages/Review.js
+++ client/src/pages/Review.js
@@ -34,10 +34,12 @@ const Review = () => {
       }
     };*/
   
-
+/*
   return (
     
-    <Formik >
+    <Formik > 
+       {() => (
+       <form  style={{ marginTop: '20px' }}>
      <Typography>Reviews</Typography>
       <Box>
         {reviews.length > 0 ? (
@@ -51,30 +53,106 @@ const Review = () => {
           <p>No reviews available</p>
         )}
       </Box>
-      {({ values, errors, touched, handleBlur, handleChange, handleSubmit, resetForm }) => (
-      <form  style={{ marginTop: '20px' }}>
+      
+      //<form  style={{ marginTop: '20px' }}>
       <TextField
                 label='Review'
-                onBlur={handleBlur}
-                onChange={handleChange}
+               // onBlur={handleBlur}
+             //   onChange={handleChange}
                // value={values.firstName}
                 name='review'
               //  error={Boolean(touched.firstName) && Boolean(errors.firstName)}
               //  helperText={touched.firstName && errors.firstName}
                 sx={{ gridColumn: 'span 2' }}
               />
-        <Button fullWidth
+       <Button
+    fullWidth
     type='submit'
-    
     sx={{
       m: '2rem 0',
       p: '1rem',
       color: 'black',
-    }} >Submit Review</Button>
-      </form> )}
+    }}
+  >
+    {'SUBMIT REVIEW'}
+  </Button>
+      </form> 
+      )}
       </Formik>
   );
+  */
+  return (
+    <Formik
+     // onSubmit={handleFormSubmit}
+      //initialValues={isLogin ? initialValuesLogin : isRegister ? initialValuesRegister : initialValuesResetPassword}
+      //validationSchema={isLogin ? loginSchema : isRegister ? registerSchema : resetPasswordSchema}
+    >
+      {({ values, errors, touched, handleBlur, handleChange, handleSubmit, resetForm }) => (
+      
+        <form >
+            <Box>
+        {reviews.length > 0 ? (
+          reviews.map((review) => (
+            <Box key={review._id} style={{ border: '1px solid #ddd', padding: '10px', margin: '10px 0' }}>
+              <p>{review.desc}</p>
+              <small>User: {review.userId}</small>
+            </Box>
+          ))
+        ) : (
+          <p>No reviews available</p>
+        )}
+      </Box>
+            <Typography fontWeight='500' variant='h5' sx={{ mb: '1.5rem' }}>
+          Reviews
+        </Typography>
+          <Box
+            display='grid'
+            gap='30px'
+            gridTemplateColumns='repeat(4, minmax(0, 1fr))'
+            sx={{
+              '& > div': { gridColumn: 'span 4' },
+            }}
+          >
+           
+  
+          
+  
+            
+                <TextField
+                  label='Your review'
+                  //type='password'
+                 // onBlur={handleBlur}
+                  //onChange={handleChange}
+                  //value={values.newPassword}
+                  name='review'
+              //    error={Boolean(touched.newPassword) && Boolean(errors.newPassword)}
+                 // helperText={touched.newPassword && errors.newPassword}
+                  sx={{ gridColumn: 'span 4' }}
+                />
+              
+            
+          </Box>
+  
+  <Box>
+    <Button
+      fullWidth
+      type='submit'
+      sx={{
+        m: '2rem 0',
+        p: '1rem',
+        color: 'black',
+      }}
+    >
+      { 'SUBMIT REVIEW'}
+    </Button>
+  
+  </Box>
+  </form>
+        )}
+      </Formik>
+    );
 };
+
 
 
 
