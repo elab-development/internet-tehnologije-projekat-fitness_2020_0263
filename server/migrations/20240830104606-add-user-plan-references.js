0bc36fd4924014b69dba8367549235d27cbf41dd
ivanaarsic <ivana.arsic83@gmail.com>
2024-08-30 13:04:05 +0200

    foreign key migracija za review ka user-u

--- server/migrations/20240830104606-add-user-plan-references.js
+++ server/migrations/20240830104606-add-user-plan-references.js
@@ -1,16 +1,15 @@
 module.exports = {
   async up(db, client) {
-    // Dodaj jedinstveni indeks za `userId` u kolekciji `plans`
+   
     await db.collection('plans').createIndex({ userId: 1 }, { unique: true });
 
-    // Ova migracija ne zahteva promene na kolekciji `users`
-    // jer `userId` referencira korisnike u kolekciji `plans`
+   
   },
 
   async down(db, client) {
-    // Ukloni jedinstveni indeks za `userId` u kolekciji `plans`
+  
     await db.collection('plans').dropIndex({ userId: 1 });
 
-    // Ova migracija ne zahteva promene na kolekciji `users`
+   
   }
 };
